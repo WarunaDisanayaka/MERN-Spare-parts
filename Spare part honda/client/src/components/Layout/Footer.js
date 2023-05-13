bf9c6d41267463112b8151eea791951797c71c48
warunahello@gmail.com <86902580+WarunaDisanayaka@users.noreply.github.com>
2023-05-13 23:11:14 +0530

    Initial commit

---
+++ Spare part honda/client/src/components/Layout/Footer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container p-4">
+        <section className="mb-4">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+            distinctio earum repellat quaerat voluptatibus placeat nam, commodi
+            optio pariatur est quia magnam eum harum corrupti dicta, aliquam
+            sequi voluptate quas.
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
